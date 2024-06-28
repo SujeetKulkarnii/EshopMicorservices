87ed2db4fbb5d8a9a742398ef01754090769ca58
SujeetKulkarnii <ssujeet.kulkarni@gmail.com>
2024-06-29 00:16:01 +0530

    commited

---
+++ eshop-microservices/src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+
+    public interface Icommand:ICommand<Unit>
+    {
+
+    }
+    public interface ICommand<out Tresponse>:IRequest<Tresponse>
+    {
+
+    }
+}
